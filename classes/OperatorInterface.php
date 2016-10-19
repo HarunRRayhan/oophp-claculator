1cc12dd9a4b04cf276bc0963064d7c690821fa1c
Harun R Rayhan <Harun R Rayhan@HarunRRayhanPC>
2016-10-19 16:57:32 +0600

    Initialize Operator Interface for all calculation Classes

---
+++ classes/OperatorInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+interface OperatorInterface {
+    
+    
+    /**
+     * Interfacing run() method
+     * @param int $result
+     * @param int $number
+     */
+    public function run($result, $number);
+}
+
