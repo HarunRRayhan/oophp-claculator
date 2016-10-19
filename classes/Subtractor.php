1a0339b117e9fac07803d19fdc83364739c7ecc2
Harun R Rayhan <Harun R Rayhan@HarunRRayhanPC>
2016-10-19 16:54:10 +0600

    Initialize Subtractor Class

---
+++ classes/Subtractor.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Subtractor class of Calculations
+ */
+class Subtractor {
+    
+    /**
+     * Running Subtractor method
+     * @param int $number
+     * @param int $result
+     * @return int
+     */
+    public function run($number, $result){
+        return $result - $number;
+    }
+}
