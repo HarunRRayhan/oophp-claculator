b4b56143676a73038f48a6a87abd94938fd0135d
Harun R Rayhan <Harun R Rayhan@HarunRRayhanPC>
2016-10-19 16:50:01 +0600

    Initialize Adder Class

---
+++ classes/Adder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Adder class
+ */
+class Adder {
+   
+    /**
+     * Running Calculation
+     * @param int $number
+     * @param int $result
+     * @return int
+     */
+    public function run($number, $result) {
+        return $result + $number;
+    }
+}
